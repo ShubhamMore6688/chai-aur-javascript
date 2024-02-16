2e37177fe3072f0bbb178def876f63356b211971
Shubham More <shubhamdmore03@gmail.com>
2024-02-16 21:56:30 +0530

    javascript basics

---
+++ 01basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 122323
+let accountName = "accountname"
+
+//avoid using var due to issue with block scope and function scope
+var accountPassword = "541223"
+accountCity = "Pune"
+
+// accountId = 2  this is not allowed
+accountName = "name"
+accountPassword = "87644"
+accountCity = "mumbai"
+console.log(accountId);
+console.table([accountId, accountName, accountPassword, accountCity])
---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,16 @@
+//ecma script standards for javascript
+let a = "hello"
+let b = 12
+
+console.log(typeof "name")
+console.log(typeof 75)
+console.log(typeof null);
+console.log(typeof undefined);
+
+//number
+//bigint
+//string
+//boolean
+//null => standalone value(empty)
+//undefined => value is not assigned yet
+//typeof null => undefined
