1878cab9a95c205f6a7fd2ec7e3c7bac7c59cd1c
Shubham More <shubhamdmore03@gmail.com>
2024-02-27 18:10:59 +0530

    async in js

---
+++ 09async/01_basics.js
@@ -0,0 +1,13 @@
+// javascript - synchronous and single threaded language
+// synchronous - each operation wait for the last one to complete before executing
+// execute one line of code at a time
+
+// blocking code vs non blocking code
+// blocking code - block the flow of the program (read file sync)
+// nonblocking code - does not block the executition (read file async)
+
+
+
+
+// when function in the call stack call the async function (settimeout) this is stored in the register call back
+// task queue is maintain for such fucntion because we need to add them again in the stack 
